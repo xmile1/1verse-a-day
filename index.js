d89dafe37f6b95f624809e2b6005a07e66b26a80
Uyiosa <hiskonxeptz@gmail.com>
2016-10-17 02:45:11 +0100

    Added a Verse for the day feature

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-
-
 unirest = require('unirest');
 
 console.log("Its a brand new day the lord has made and it is marvelous in our sight");
-console.log("Please wait while I retrieve your verse for the day is");
+console.log("Please wait while I retrieve your verse for the day");
 getVerse("random");
-
-
 
 //John+3:16-17
 function getVerse(passage){

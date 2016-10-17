bf8c6861ec235676a405b62ef0629b33a31fe30e
Uyiosa <hiskonxeptz@gmail.com>
2016-10-17 02:40:00 +0100

    Twitter Failed, Changed My API to net bible API

--- index.js
+++ index.js
@@ -1,34 +1,17 @@
-//Callback functions
-    var error = function (err, response, body) {
-        console.log('ERROR [%s]', err);
-    };
-    var success = function (data) {
-        console.log('Data [%s]', data);
-    };
 
-    var Twitter = require('twitter-js-client').Twitter;
 
-    //Get this data from your twitter apps dashboard
-    var config = {
-        "consumerKey": "3balG7SyPNgXuXgIC2uf5RMKx",
-        "consumerSecret": "ANjsOeZ5E0ugaYITm4CXcJR1SMHHN960aexOV6TEqlyAO2KJF1",
-        "accessToken": "XXX",
-        "accessTokenSecret": "XXX",
-        "callBackUrl": "XXX"
-    }
+unirest = require('unirest');
 
-    var twitter = new Twitter(config);
+console.log("Its a brand new day the lord has made and it is marvelous in our sight");
+console.log("Please wait while I retrieve your verse for the day is");
+getVerse("random");
 
-    
 
-    //
-    // Get 10 tweets containing the hashtag haiku
-    //
 
-    twitter.getSearch({'q':'#haiku','count': 10}, error, success);
-
-    //
-    // Get 10 popular tweets with a positive attitude about a movie that is not scary 
-    //
-
-    twitter.getSearch({'q':' movie -scary :) since:2013-12-27', 'count': 10, 'result\_type':'popular'}, error, success);
+//John+3:16-17
+function getVerse(passage){
+    unirest.get("http://labs.bible.org/api/?passage="+ passage + "&formatting=plain")
+.end(function (result) {
+  console.log(result.body);
+});
+}

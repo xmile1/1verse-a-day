56bed84c650cb590173ec4dd50f7c081a46f3522
Uyiosa <hiskonxeptz@gmail.com>
2016-10-17 03:02:00 +0100

    Added a search a Bible passage feature

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 unirest = require('unirest');
 
+  var prompt = require('prompt');
+
+
+
 console.log("Its a brand new day the lord has made and it is marvelous in our sight");
 console.log("Please wait while I retrieve your verse for the day");
 getVerse("random");
+console.log("I hope that blessed you!");
+console.log("Do you want to search for a bible passage?");
+console.log("Please enter your verse in the format John+3:12-20.");
+console.log("Please be careful i am not protected from XSS attack");
+promptUser(["verse"]);
+
 
 //John+3:16-17
 function getVerse(passage){
@@ -11,3 +21,20 @@ function getVerse(passage){
   console.log(result.body);
 });
 }
+
+
+function promptUser(inputIdentity){
+    
+  prompt.start();
+  prompt.get(inputIdentity, function (err, result) {
+    if (err) { return onErr(err); }
+    getVerse(result[inputIdentity[0]]);
+  });
+
+}
+
+
+  function onErr(err) {
+    console.log(err);
+    return 1;
+  }
